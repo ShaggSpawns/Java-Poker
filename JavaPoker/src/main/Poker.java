8368d1cd2a8b837ef7abd32d9a928ae5eb3a18b5
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-09 19:00:26 -0400

    Changes to PokerUtils

--- JavaPoker/src/main/Poker.java
+++ JavaPoker/src/main/Poker.java
@@ -8,44 +8,4 @@ public class Poker {
 		Scanner kb = new Scanner(System.in);
 		shuffle();
 	}
-
-	fuck you
-	public static card[] shuffle() {
-		card[] shuffled = new card[];
-		card[] shuffleThis = new card[52];
-		int cardNum = 1;
-		int loc;
-		int position = 0;
-		
-		for(int i = 0; i < 52; i++) {
-			shuffleThis[i] = cardNum;
-			cardNum++;
-			if(cardNum == 14)
-				cardNum = 1;
-		}
-		
-		for(int a = 0; a < 52; a++)
-			shuffled[a] = 0;
-		
-		System.out.print("Shuffling, please wait");
-		
-		for(int i = 0; i < 52; i++) {
-			loc = (int)(Math.random()*52);
-			
-			if(i%4 == 0)
-				System.out.print(".");
-			
-			if(shuffled[loc] == 0) {
-				shuffled[loc] = shuffleThis[position];
-				position++;
-			}
-			else
-				i--;
-		}
-		for(int i = 0; i < 52; i ++)
-			if(shuffled[i] == 0)
-				shuffled[i] = 99;
-		return shuffled;
-	}
-
 }
