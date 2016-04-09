011722644b84c0bb90c06715b0cadfb37205fb34
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-09 19:59:28 -0400

    asd

--- JavaPoker/src/main/Player.java
+++ JavaPoker/src/main/Player.java
@@ -1,5 +1,15 @@
 package main;
 
-public class Player {
 
+public class Player {
+	private double chipCount;
+	int playTimes = 0;
+	
+	Player(String name) { 
+		newChipCount(3000);
+	}
+	
+	private void newChipCount(double winnings) {
+		chipCount += winnings;
+	}
 }
--- JavaPoker/src/main/Poker.java
+++ JavaPoker/src/main/Poker.java
@@ -1,8 +1,13 @@
 package main;
 import java.util.Scanner;
+import java.util.ArrayList;
+
+//TODO Score Method, Chips, Betting, Orbit Count
 
 public class Poker {
 	public static void main(String[] args) {
+		
+		ArrayList<Card> board = new ArrayList<Card>();
 		Scanner kb = new Scanner(System.in);
 		String userInput;
 		boolean playMore = true;
