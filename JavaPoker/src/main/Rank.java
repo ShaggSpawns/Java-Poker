2ddf2ff2fd7f1d4ea6ca8e2f8b32ecdce73513dd
Shaggy <shaggspawns@gmail.com>
2016-04-09 18:03:06 -0500

    Rank update

--- JavaPoker/src/main/Rank.java
+++ JavaPoker/src/main/Rank.java
@@ -1,5 +1,23 @@
 package main;
 
 public enum Rank {
+	Ace(1, "Ace"), Two(2, "Two"), Three(3, "Three"), Four(4, "Four"), Five(5, "Five"),
+	Six(6, "Six"), Seven(7, "Seven"), Eight(8, "Eight"), Nine(9, "Nine"), Ten(10, "Ten"),
+	Jack(11, "Jack"), Queen(12, "Queen"), King(13, "King");
 
+    private int value;
+    private String name;
+
+    Rank(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+    
+    public int getNumVal() {
+        return value;
+    }
+    
+    public String toString() {
+    	return name;
+    }
 }
