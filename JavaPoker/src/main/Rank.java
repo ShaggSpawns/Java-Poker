04ec6fd47b5f3b3f72c8575c6e5f2b95b5c0d351
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-09 19:07:12 -0400

    asd

--- JavaPoker/src/main/Rank.java
+++ JavaPoker/src/main/Rank.java
@@ -1,9 +1,9 @@
 package main;
 
 public enum Rank {
-	Ace(1, "Ace"), Two(2, "2"), Three(3, "3"), Four(4, "4"), Five(5, "5"),
+	Ace(1, "A"), Two(2, "2"), Three(3, "3"), Four(4, "4"), Five(5, "5"),
 	Six(6, "6"), Seven(7, "7"), Eight(8, "8"), Nine(9, "9"), Ten(10, "10"),
-	Jack(11, "Jack"), Queen(12, "Queen"), King(13, "King");
+	Jack(11, "J"), Queen(12, "Q"), King(13, "K");
 
     private int value;
     private String name;
