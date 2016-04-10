dec86c3e4771db82c39d4e05c63895282677e3c7
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-10 00:24:25 -0400

    asd

--- JavaPoker/src/main/Poker.java
+++ JavaPoker/src/main/Poker.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 public class Poker {
 	public static void main(String[] args) {
 		
-		ArrayList<Card> board = new ArrayList<Card>();
 		ArrayList<Player> playerList = new ArrayList<Player>();
+		Board board;
 		Scanner kb = new Scanner(System.in);
 		String userInput;
 		int numPlayers;
 		boolean playMore = true;
+		boolean noBroke = true;
+		boolean p1 = false;
+		boolean p2 = false;
 		
 		System.out.println("Would you like to play some POKER!");
 		userInput = kb.nextLine().toUpperCase();
@@ -24,15 +27,26 @@ public class Poker {
 		numPlayers = kb.nextInt();
 		kb.nextLine();
 		
-		while(playMore) {
-			for(int i = 1; i < numPlayers+1; i++) {
-				System.out.println("Enter in player " + i + "'s name:");
-				playerList.add(new Player(kb.nextLine()));
+		for(int i = 1; i < numPlayers+1; i++) {
+			System.out.println("Enter in player " + i + "'s name:");
+			playerList.add(new Player(kb.nextLine()));
+		}
+		
+		board = new Board(playerList);
+		
+		while(noBroke) {
+			for(int i = 0; i < playerList.size(); i++) {
+				if(p1 == true)
+					if(playerList.get(i).getChipCount() > 0)
+						p2 = true;
+				if(p1 == false)
+					if(playerList.get(i).getChipCount() > 0)
+						p1 = true;	
 			}
-			new Board(playerList);
-			
-			//TODO input parameters
-			//PokerUtils.score();
+			if(p2 != true) { 
+				noBroke = false;
+				break;
+			}
 		}
 	}
 }
