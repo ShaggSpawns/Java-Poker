c32b0055f4c222b8dcbc107cb450e88e70247abe
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-10 13:10:51 -0400

    Changes to main method. Need to be moved to board

--- JavaPoker/src/main/Poker.java
+++ JavaPoker/src/main/Poker.java
@@ -6,17 +6,29 @@ import java.util.ArrayList;
 
 public class Poker {
 	public static String userInput;
+	public static int numHandsPlayed = 0;
+	public static int bigBlind, smallBlind;
 	public static boolean playMore = true;
 	public static Scanner kb = new Scanner(System.in);
 	public static Board board;
 	public static ArrayList<Player> playerList = new ArrayList<Player>();
-	public static final double ANTE = 100;
+	public static double ANTE = 100; //Antes change based on number of hands that have been played. There are also Big Blinds and Small Blinds
+								     //Big blinds in the opening (ante:100) will be 800 (9 players playing, 8 players ante'd)
+									 //Small blinds are always half the Big blind. So opening will be (small) 400/800 (big) ante 100
 	
 	public static void main(String[] args) {
 		while(playMore) {
 			introSequence();
 			while(playerList.size() > 1) {
 				board = new Board(playerList, ANTE);
+				
+				numHandsPlayed++;
+				
+				//TODO move this to board class and make players pay these amounts every 8 rounds. IE. Round 1, player 1 pays BB. Player 9 pays SB
+				//players 2-8 pay ante. Round 2, Player 2 pays BB, Player 1 pays SB, others pay ante. Etc.
+				if(numHandsPlayed%4 == 0) ANTE += 50;
+				bigBlind = (int) (ANTE/playerList.size()-1);
+				smallBlind = bigBlind/2;
 			}
 			endSequence();
 		}
@@ -36,8 +48,6 @@ public class Poker {
 			System.out.println("Enter in player " + i + "'s name:");
 			playerList.add(new Player(kb.nextLine()));
 		}
-		
-		board = new Board(playerList, ANTE);
 	}
 	
 	public static void endSequence() {
