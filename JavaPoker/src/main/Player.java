3e9cf594cec6ded802fc8d8afab6861faf32b1a3
Shaggy <shaggspawns@gmail.com>
2016-04-09 22:31:50 -0500

    Player

--- JavaPoker/src/main/Player.java
+++ JavaPoker/src/main/Player.java
@@ -3,13 +3,40 @@ package main;
 
 public class Player {
 	private double chipCount;
-	int playTimes = 0;
+	private int playTimes = 0; // TODO Save player data to file
 	
 	Player(String name) { 
-		newChipCount(3000);
+		setChipCount(3000);
 	}
 	
-	private void newChipCount(double winnings) {
-		chipCount += winnings;
+	Player(String name, double chipCount) {
+		setChipCount(chipCount);
+	}
+	
+	public void newChipCount(double winnings) {
+		setChipCount(chipCount + winnings);
+	}
+
+	public int getPlayTimes() {
+		return playTimes;
+	}
+
+	public void setPlayTimes(int playTimes) {
+		this.playTimes = playTimes;
+	}
+
+	public double getChipCount() {
+		return chipCount;
+	}
+
+	private void setChipCount(double chipCount) {
+		if (chipCount > 0)
+			this.chipCount = chipCount;
+		else
+			try {
+				throw new Exception("Broke");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 }
