84452fab4a6f15a36ce96e6f89d6094253da3390
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-10 14:15:50 -0400

    asd

--- JavaPoker/src/main/PokerUtils.java
+++ JavaPoker/src/main/PokerUtils.java
@@ -12,7 +12,29 @@ public class PokerUtils{
 	public int score(Card[] playerHand, ArrayList<Card> boardCards) {
 		int cardSelector = 0;
 		boolean pair = (pair(playerHand, boardCards) || handPair(playerHand));
+		boolean handPair = checkHandPair(playerCards);
+		boolean twoPair = false;
+		boolean threeOfKind = false;
+		boolean fourOfKind = false;
+		boolean fullHouse = false;
+		boolean straight = checkStraight(playerHand, straightCards);
+		boolean flush = checkFlush(playerHand, boardCards);
+		boolean straightFlush = false;
 		
+		if(straightFlush)
+			return 2;
+		if(fourOfKind)
+			return 3;
+		if(fullHouse)
+			return 4;
+		if(flush)
+			return 5;
+		if(straight)
+			return 6;
+		if(threeOfKind)
+			return 7;
+		if(twoPair)
+			return 8;
 		if(pair)
 			return 9;
 		return 10;
@@ -46,6 +68,23 @@ public class PokerUtils{
 			numOfCards++;
 		
 		if(numOfCards >= 3)
+			return true;
+		return false;
+	}
+	
+	public boolean checkFlush(Card[] playerHand, ArrayList<Card> boardCards) {
+		int numOfCards = 1;
+		for(int i = 0; i < 5; i++)
+			if(playerHand[0].getSuit() == boardCards.get(i).getSuit())
+				numOfCards++;
+		if(numOfCards >= 5)
+			return true;
+		
+		numOfCards = 1;
+		for(int i = 0; i < 5; i++)
+			if(playerHand[1].getSuit() == boardCards.get(i).getSuit())
+				numOfCards++;
+		if(numOfCards >= 5)
 			return true;
 		return false;
 	}
