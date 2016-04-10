b7385c2bc8dde3cc8b1330f38cd7bbda18cb10cb
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-09 23:58:25 -0400

    asd

--- JavaPoker/src/main/PokerUtils.java
+++ JavaPoker/src/main/PokerUtils.java
@@ -5,56 +5,6 @@ import java.io.*;
 //TODO Score method
 
 public class PokerUtils extends Poker{
-	public static Card[] shuffle() {
-		Card[] shuffled = new Card[52];
-		Card[] shuffleThis = new Card[52];
-		int cardNum = 1;
-		int suit = 1;
-		int loc;
-		int position = 0;
-		int k = 0;
-		Suit cardSuit = Suit.Club;
-		
-		for(int i = 0; i < 52; i++) {
-			switch(suit) {
-			case 2:
-				cardSuit = Suit.Spade;
-				break;
-			case 3:
-				cardSuit = Suit.Diamond;
-				break;
-			case 4:
-				cardSuit = Suit.Heart;
-				break;
-			default:
-				break;
-			}
-			
-			shuffleThis[i] = new Card(Rank.values()[k], cardSuit);
-			k++;
-			cardNum++;
-			if(k == 13) k = 0;
-			if(cardNum == 14) {
-				suit++;
-				cardNum = 1;
-			}
-		}
-		for(int a = 0; a < 52; a++)
-			shuffled[a] = new Card(Rank.values()[13], cardSuit);
-		
-		for(int i = 0; i < 52; i++) {
-			loc = (int)(Math.random()*52);
-			
-			if(shuffled[loc].getRank() == Rank.values()[13]) {
-				shuffled[loc] = shuffleThis[position];
-				position++;
-			}
-			else
-				i--;
-		}
-		return shuffled;
-	}
-	
 	//TODO parameters (board, player hands)
 	public static int score(Card[] playerHand, ArrayList<Card> boardCards) {
 		Card[] board = (Card[]) boardCards.toArray();
