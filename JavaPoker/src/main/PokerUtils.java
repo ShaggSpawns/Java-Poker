984ab887670129ee683f4b44bed2be6f46b28bec
Jacob <Jacob@DESKTOP-L2N540Q>
2016-04-10 14:20:57 -0400

    asd

--- JavaPoker/src/main/PokerUtils.java
+++ JavaPoker/src/main/PokerUtils.java
@@ -9,10 +9,10 @@ public class PokerUtils{
 	public int numGoodCards = 0;
 	public boolean handPairs = false;
 	
-	public int score(Card[] playerHand, ArrayList<Card> boardCards) {
+	public int score(ArrayList<Card> playerHand, ArrayList<Card> boardCards) {
 		int cardSelector = 0;
 		boolean pair = (pair(playerHand, boardCards) || handPair(playerHand));
-		boolean handPair = checkHandPair(playerCards);
+		boolean handPair = handPair(playerHand);
 		boolean twoPair = false;
 		boolean threeOfKind = false;
 		boolean fourOfKind = false;
@@ -40,16 +40,16 @@ public class PokerUtils{
 		return 10;
 	}
 	
-	public boolean handPair(Card[] playerHand) {
-		if(playerHand[0].getRank() == playerHand[1].getRank())
+	public boolean handPair(ArrayList<Card> playerHand) {
+		if(playerHand.get(0).getRank() == playerHand.get(1).getRank())
 			return true;
 		return false;
 	}
 	
-	public boolean pair(Card[] playerHand, ArrayList<Card> boardCards) { 
+	public boolean pair(ArrayList<Card> playerHand, ArrayList<Card> boardCards) { 
 		for(int i = 0; i < 2; i++)
 			for(int k = 0; k < 5; k++)
-				if(playerHand[i].getRank() == boardCards.get(k).getRank()) {
+				if(playerHand.get(i).getRank() == boardCards.get(k).getRank()) {
 					goodCards[i] = 1;
 					numGoodCards++;
 					return true;
@@ -72,17 +72,17 @@ public class PokerUtils{
 		return false;
 	}
 	
-	public boolean checkFlush(Card[] playerHand, ArrayList<Card> boardCards) {
+	public boolean checkFlush(ArrayList<Card> playerHand, ArrayList<Card> boardCards) {
 		int numOfCards = 1;
 		for(int i = 0; i < 5; i++)
-			if(playerHand[0].getSuit() == boardCards.get(i).getSuit())
+			if(playerHand.get(0).getSuit() == boardCards.get(i).getSuit())
 				numOfCards++;
 		if(numOfCards >= 5)
 			return true;
 		
 		numOfCards = 1;
 		for(int i = 0; i < 5; i++)
-			if(playerHand[1].getSuit() == boardCards.get(i).getSuit())
+			if(playerHand.get(1).getSuit() == boardCards.get(i).getSuit())
 				numOfCards++;
 		if(numOfCards >= 5)
 			return true;
