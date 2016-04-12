0a019ff9aa73afc4c610bb2bc4f0ffefce06855f
Shaggy <shaggspawns@gmail.com>
2016-04-11 19:01:05 -0500

    Update

--- JavaPoker/src/gui/RunnerGUI.java
+++ JavaPoker/src/gui/RunnerGUI.java
@@ -3,6 +3,8 @@ package gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -15,33 +17,38 @@ public class RunnerGUI extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		BorderPane border = new BorderPane();
-        border.setCenter(launchSceen());
+		BorderPane root = new BorderPane();
+		root.setCenter(launchScreen(root));
  
-        Scene scene = new Scene(border, 1280, 720);
-        
+        Scene scene = new Scene(root, 1280, 720);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Seven-Card Stud Poker");
         primaryStage.show();
 	}
 	
-	private GridPane launchSceen() {
+	private GridPane launchScreen(BorderPane root) {
 		GridPane grid = new GridPane();
-		Button play = new Button("Play");
-		Button host = new Button("Host and Play");
-		grid.add(play, 0, 0);
-		grid.add(host, 0, 1);
+		Button playBtn = new Button("Play");
+		Button hostBtn = new Button("Host and Play");
+		grid.add(playBtn, 0, 0);
+		grid.add(hostBtn, 0, 1);
+
+		playBtn.setOnAction(e -> root.setCenter(playScreen()));
+		hostBtn.setOnAction(e -> root.setCenter(hostScreen()));
 		return grid;
 	}
 	
-	private GridPane playSceen() {
+	private GridPane playScreen() {
 		GridPane grid = new GridPane();
-		
+		grid.add(new Label("Playing"), 0, 0);
 		return grid;
 	}
 	
 	private TabPane hostScreen() {
 		TabPane tab = new TabPane();
+		Tab playTab = new Tab();
+		playTab.setContent(playScreen());
+		Tab hostTab = new Tab();
 		return tab;
 	}
 }
